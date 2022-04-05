f818ed278499c2e2ea8361584bfad19f80d2d8e4
Ben Johnson <johnsonb@ucar.edu>
2022-04-05 08:23:31 -0600

    Move shr to subdirectory

--- src/shr/shr_kind_mod.F90
+++ src/shr/shr_kind_mod.F90
@@ -1,12 +1,15 @@
-module precision
-   implicit none
-   !-----------------------------------------------------------------
-   ! Define precision constants for consistency across modules
-   !-----------------------------------------------------------------
-   
-   integer, public, parameter :: r8 = selected_real_kind(12) ! 8 byte real
-   integer, public, parameter :: r4 = selected_real_kind(6)  ! 4 byte real
-   integer, public, parameter :: i8 = selected_int_kind(13)  ! 8 byte integer
-   integer, public, parameter :: i4 = selected_int_kind(6)   ! 4 byte integer
 
-end module precision
+MODULE shr_kind_mod
+
+   !----------------------------------------------------------------------------
+   ! precision/kind constants add data public
+   !----------------------------------------------------------------------------
+   public
+   integer,parameter :: SHR_KIND_R8 = selected_real_kind(12) ! 8 byte real
+   integer,parameter :: SHR_KIND_R4 = selected_real_kind( 6) ! 4 byte real
+   integer,parameter :: SHR_KIND_RN = kind(1.0)              ! native real
+   integer,parameter :: SHR_KIND_I8 = selected_int_kind (13) ! 8 byte integer
+   integer,parameter :: SHR_KIND_I4 = selected_int_kind ( 6) ! 4 byte integer
+   integer,parameter :: SHR_KIND_IN = kind(1)                ! native integer
+
+END MODULE shr_kind_mod
