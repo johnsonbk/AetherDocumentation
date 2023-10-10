c94c397dda083ddde8253650d3d1c63684868bc5
Ben Johnson <johnsonb@ucar.edu>
2023-10-10 09:20:45 -0600

    Add scripts directory

---
+++ scripts/plot_cube.py
@@ -0,0 +1,30 @@
+#!/opt/local/bin/python
+from __future__ import print_function
+
+import numpy as np
+import netCDF4
+import matplotlib.pyplot as plt
+from math import pi
+
+directory = '../restarts/restartOut.Cube.1member/'
+files = [directory+'grid_g' + str(side).zfill(4) + '.nc' for side in range(0,6)]
+print(files)
+
+colors = ['#0072b2', '#e69f00', '#56b4e9', '#009e73', '#f0e442', '#cc79a7']
+
+radians_to_degrees = 180.0/pi
+
+fig, ax = plt.subplots()
+
+for ifile, this_file in enumerate(files):
+    f = netCDF4.Dataset(this_file)
+    lons = f.variables['Longitude'][:]*radians_to_degrees
+    lats = f.variables['Latitude'][:]*radians_to_degrees
+    ax.scatter(lons, lats, c=colors[ifile], edgecolors='none', label='Grid ' + str(ifile).zfill(4))
+    f.close()
+
+ax.legend()
+plt.title('')
+plt.ylabel('Latitude')
+plt.xlabel('Longitude')
+plt.savefig('scatter.png', dpi=200)
