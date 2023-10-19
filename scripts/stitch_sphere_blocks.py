62b73cc4c5e4ca6c5991e42bf82ffe20f5d4aa20
Benjamin Johnson <johnsonb@ucar.edu>
2023-10-18 21:00:47 -0600

    Add script to stitch blocks from lat/lon sphere

---
+++ scripts/stitch_sphere_blocks.py
@@ -0,0 +1,9 @@
+#!/opt/local/bin/python
+from __future__ import print_function
+
+import numpy as np
+import netCDF4
+import matplotlib.pyplot as plt
+from math import pi
+
+filepath = '../outputs/output.Sphere.2members/3DALL_20110320_000000_m0000.nc'
