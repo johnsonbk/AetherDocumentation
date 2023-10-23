64d4a38bd578d490e506826fb6d127ba2a200c55
Benjamin Johnson <johnsonb@ucar.edu>
2023-10-23 08:13:31 -0600

    Update stitch_sphere_blocks

--- scripts/stitch_sphere_blocks.py
+++ scripts/stitch_sphere_blocks.py
@@ -6,7 +6,9 @@ import netCDF4
 import matplotlib.pyplot as plt
 from math import pi
 
-filepath = '../outputs/output.Sphere.2members/3DALL_20110320_000000_m0000.nc'
+# filepath = '../outputs/output.Sphere.2members/3DALL_20110320_000000_m0000.nc'
+input_filepath = '../outputs/output.Sphere.2members/3DALL_20110320_003000_m0000.nc'
+output_filepath = '../outputs/output.Sphere.2members/3DALL_20110320_003000_m0000_stitched.nc'
 
 # This script takes an output file from Aether and stitches the blocks together
 # to create a single 3-d array with dimensions of [lon, lat, z].
@@ -15,23 +17,64 @@ filepath = '../outputs/output.Sphere.2members/3DALL_20110320_000000_m0000.nc'
 # interfaces of the blocks. It discards the first and last two rows and columns
 # from each block.
 
-f = netCDF4.Dataset(filepath)
+input_file = netCDF4.Dataset(input_filepath)
+output_file = netCDF4.Dataset(output_filepath, mode='w')
 
 ntruncate = 2
-nblocks = len(f.dimensions['block'])
-nlons_in_block = len(f.dimensions['lon'])
-nlats_in_block = len(f.dimensions['lat'])
-nzs = len(f.dimensions['z'])
+nblocks = len(input_file.dimensions['block'])
+nlons_in_block = len(input_file.dimensions['lon'])
+nlats_in_block = len(input_file.dimensions['lat'])
+nzs = len(input_file.dimensions['z'])
 
 # The "2*nblocks/2" syntax looks silly but it works and may be forward compatible if more blocks are introduced
-nlons = nlons_in_block*nblocks/2-ntruncate*nblocks/2
-nlats = nlats_in_block*nblocks/2-ntruncate*nblocks/2
+nlons = int(nlons_in_block*nblocks/2-2*ntruncate*nblocks/2)
+nlats = int(nlats_in_block*nblocks/2-2*ntruncate*nblocks/2)
+print('nlons', nlons)
+print('nlats', nlats)
 
-stitched_array = np.empty((nlons, nlats, nzs))
+# Write dimensions to the output file
+lon_dim = output_file.createDimension('lon', nlons)
+lat_dim = output_file.createDimension('lat', nlats)
+z_dim = output_file.createDimension('z', nzs)
+time_dim = output_file.createDimension('time', None)
 
-o_plus = f.variables['O+']
+# Write time variable to output file
+time = output_file.createVariable('time', np.double, ('time',))
 
-for iblock in range(0, nblocks):
-    if iblock == 0:
-        stitched_array[0:nlons_in_block-ntruncate, 0:nlats_in_block-ntruncate, :] = o_plus[iblock, ntruncate:-ntruncate, ntruncate:-ntruncate]
-    
+for ikey in input_file.variables:
+    if ikey == 'time':
+        pass
+    else:
+        stitched_array = np.empty((nlons, nlats, nzs))
+
+        input_field = input_file.variables[ikey]
+
+        for iblock in range(0, nblocks):
+            print('stitched_array[0:nlons_in_block-ntruncate, 0:nlats_in_block-ntruncate, :].shape()', stitched_array[0:nlons_in_block-2*ntruncate, 0:nlats_in_block-2*ntruncate, :].shape)
+            print('input_field[iblock, ntruncate:-ntruncate, ntruncate:-ntruncate, :].shape()', input_field[iblock, ntruncate:-ntruncate, ntruncate:-ntruncate, :].shape)
+            
+            if iblock == 0:
+                stitched_array[0:nlons_in_block-2*ntruncate, 0:nlats_in_block-2*ntruncate, :] = input_field[iblock, ntruncate:-ntruncate, ntruncate:-ntruncate, :]
+            elif iblock == 1:
+                stitched_array[nlons_in_block-2*ntruncate:, 0:nlats_in_block-2*ntruncate, :] = input_field[iblock, ntruncate:-ntruncate, ntruncate:-ntruncate, :]
+            elif iblock == 2:
+                stitched_array[0:nlons_in_block-2*ntruncate, nlats_in_block-2*ntruncate: :] = input_field[iblock, ntruncate:-ntruncate, ntruncate:-ntruncate, :]
+            elif iblock == 3:
+                stitched_array[nlons_in_block-2*ntruncate:, nlats_in_block-2*ntruncate: :] = input_field[iblock, ntruncate:-ntruncate, ntruncate:-ntruncate, :]
+
+        output_field = output_file.createVariable(ikey, np.float32, ('lon','lat','z'))
+        output_field.units = input_field.units
+        output_field.long_name = input_field.units
+        output_field[:] = stitched_array
+
+output_file.close()
+
+plt.pcolor(stitched_array[:, :, -1], vmin=stitched_array[:, :, -1].min(), vmax=stitched_array[:, :, -1].max())
+plt.colorbar()
+plt.savefig('stitched_array.png')
+
+plt.clf()
+
+plt.pcolor(input_field[2, :, :, -1], vmin=input_field[2, :, :, -1].min(), vmax=input_field[2, :, :, -1].max())
+plt.colorbar()
+plt.savefig('pcolor_on_input_field.png')
